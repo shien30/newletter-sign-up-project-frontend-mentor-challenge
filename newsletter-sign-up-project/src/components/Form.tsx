6f1bbca3e047c19725dc1f79c06a2629a5115432
shien30 <shienloh504@gmail.com>
2026-05-13 13:59:01 +0100

    created a form component

---
+++ newsletter-sign-up-project/src/components/Form.tsx
@@ -0,0 +1,19 @@
+ export default function Form(){
+ return(
+        <div>
+          <form>
+        <label htmlFor="email">Email Address</label><br></br>
+        <input type="email"
+                id="email"
+                name="email"
+                placeholder="email@company.com"
+                /><br></br>
+
+       <button  type="submit"> Subscribe to monthly newsletter</button>
+       </form>      
+        </div>
+      
+ )
+ 
+ }
+ 
