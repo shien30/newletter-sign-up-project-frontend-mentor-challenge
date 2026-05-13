3b835eae016f713726698798e742f670dbee9b68
shien30 <shienloh504@gmail.com>
2026-05-13 14:00:52 +0100

    created a image components

---
+++ newsletter-sign-up-project/src/components/Image.tsx
@@ -0,0 +1,11 @@
+ 
+ import illustrationSignUpDesktop from "../images/illustration-sign-up-desktop.svg"
+
+export default function Image(){
+       return(
+       <div className="image-illustration">
+        <img src={illustrationSignUpDesktop} alt=""/>
+       </div>
+       )
+}
+       
