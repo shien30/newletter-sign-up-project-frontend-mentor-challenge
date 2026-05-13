41e1f93fc929a88dc8656dbf5fe7419fcfa80171
shien30 <shienloh504@gmail.com>
2026-05-13 13:56:42 +0100

    created Newsletter component

---
+++ newsletter-sign-up-project/src/components/NewsLetter.tsx
@@ -0,0 +1,19 @@
+
+import iconList from"../images/icon-list.svg"
+
+
+export default function NewLetter(){
+     return(
+        <div className="newsletter-container">
+
+        <h1> Stay updated!</h1>
+        <p>Join 60,000+ product managers receiving monthly updates on:</p>
+        <ul>
+        <li><span><img src={iconList} alt="" /></span>Product discovery and building what matters</li>
+        <li><span><img src={iconList} alt="" /></span>Measuring to ensure updates are a success </li>
+        <li><span><img src={iconList} alt="" /></span>And much more!</li>
+        </ul>
+        </div>
+        
+    )
+}
